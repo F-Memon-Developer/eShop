aba1530f5f540ec7e67b2163cad16f3313fe1ca6
Memon <memon2572006f@gmail.com>
2025-09-05 09:36:03 +0500

    config.js

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -4,12 +4,12 @@ import { getFirestore } from "firebase/firestore";
 import { getStorage } from "firebase/storage";
 
 export const firebaseConfig = {
-  apiKey: "AIzaSyDXnWxzGFCcWD8c03Xk7HPcxeGl9CTwubg",
-  authDomain: "eshop-f1bdf.firebaseapp.com",
-  projectId: "eshop-f1bdf",
-  storageBucket: "eshop-f1bdf.firebasestorage.app",
-  messagingSenderId: "44163001453",
-  appId: "1:44163001453:web:8b97dcd80501efc4ae6364"
+  apiKey: "Use your own",
+  authDomain: "eshop",
+  projectId: "eshop",
+  storageBucket: "eshop.app",
+  messagingSenderId: "",
+  appId: ""
 };
 
 // Initialize Firebase
@@ -19,3 +19,4 @@ export const db = getFirestore(app)
 export const storage = getStorage(app)
 
 export default app;
+
